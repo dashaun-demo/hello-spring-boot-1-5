6e02f9f017f37760101a8a9e0e5c1c2f8a6a99eb
dashaun <dashaun@dashaun.com>
2023-11-29 09:19:46 -0600

    Because we can't build Spring Boot 1.5.x from start.spring.io

---
+++ src/test/java/dev/dashaun/kubed/spring/hello/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dashaun.kubed.spring.hello.hellospring;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
