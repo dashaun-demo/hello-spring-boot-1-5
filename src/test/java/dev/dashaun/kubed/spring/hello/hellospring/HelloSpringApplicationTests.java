463b83206eed677d61fb5d3aeb234e8cdaff6e99
DaShaun Carter <dashaun@dashaun.com>
2024-11-13 08:56:02 -0500

    make public

--- src/test/java/dev/dashaun/kubed/spring/hello/hellospring/HelloSpringApplicationTests.java
+++ src/test/java/dev/dashaun/kubed/spring/hello/hellospring/HelloSpringApplicationTests.java
@@ -4,10 +4,10 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HelloSpringApplicationTests {
+public class HelloSpringApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
